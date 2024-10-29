ed5320dc62311940f4d5d9ae1c3347a39c24bffd
Vinyl C <vinyl.chintalapudi@gmail.com>
2024-10-29 17:20:35 +0900

    update page - update team section

--- src/types/team.ts
+++ src/types/team.ts
@@ -3,7 +3,5 @@ export type TeamType = {
   name: string;
   designation: string;
   image: string;
-  facebookLink: string;
-  twitterLink: string;
-  instagramLink: string;
+  linkedinLink: string;
 };
