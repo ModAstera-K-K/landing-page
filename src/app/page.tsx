919c268d5e6aedc4b76489e64d314f8ab267cb62
Vinyl C <vinyl.chintalapudi@gmail.com>
2024-10-29 15:46:44 +0900

    update page - comment out unused parts and update contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import CallToAction from "@/components/CallToAction";
 import Clients from "@/components/Clients";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
-import Faq from "@/components/Faq";
+// import Faq from "@/components/Faq";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
+// import Pricing from "@/components/Pricing";
 import Team from "@/components/Team";
-import Testimonials from "@/components/Testimonials";
+// import Testimonials from "@/components/Testimonials";
 import { getAllPosts } from "@/utils/markdown";
 import { Metadata } from "next";
 
