96293edae18e97f1e08b978b870a0043b53c9db4
Joshua Owoyemi <tjosh.owoyemi@gmail.com>
2024-10-30 17:42:16 +0900

    update features section

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -9,21 +9,21 @@ const CallToAction = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[570px] text-center">
                 <h2 className="mb-2.5 text-3xl font-bold text-white md:text-[38px] md:leading-[1.44]">
-                  <span>What Are You Looking For?</span>
+                  <span>What are your HealthTech needs?</span>
                   <span className="text-3xl font-normal md:text-[40px]">
                     {" "}
-                    Get Started Now{" "}
+                    We can help you get started.
                   </span>
                 </h2>
                 <p className="mx-auto mb-6 max-w-[515px] text-base leading-[1.5] text-white">
-                  There are many variations of passages of Lorem Ipsum but the
-                  majority have suffered in some form.
+                  Schedule an introductory call with us to discuss your
+                  HealthTech needs.
                 </p>
                 <Link
                   href="/"
                   className="inline-block rounded-md border border-transparent bg-secondary px-7 py-3 text-base font-medium text-white transition hover:bg-[#0BB489]"
                 >
-                  Start using Play
+                  Talk to Us
                 </Link>
               </div>
             </div>
