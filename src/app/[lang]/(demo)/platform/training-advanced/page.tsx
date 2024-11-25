a5276304511f441f9753fa2917a77f52fec236ae
Vinyl C <vinyl.chintalapudi@gmail.com>
2024-11-25 23:48:16 +0900

    ui fix - advanced training

--- src/app/[lang]/(demo)/platform/training-advanced/page.tsx
+++ src/app/[lang]/(demo)/platform/training-advanced/page.tsx
@@ -127,6 +127,7 @@ export default function AdvancedView() {
           onNodesChange={onNodesChange}
           onEdgesChange={onEdgesChange}
           fitView
+          fitViewOptions={{ padding: 0.3 }} // Adjust zoom level
           style={{ width: "100%", height: "100%" }}
           onNodeClick={handleNodeClick}
         >
