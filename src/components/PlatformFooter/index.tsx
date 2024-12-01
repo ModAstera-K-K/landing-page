3ed375a83855403c88c4414ea2585adf8261e0aa
Joshua Owoyemi <tjosh.owoyemi@gmail.com>
2024-12-02 00:58:53 +0900

    minor refactor

--- src/components/PlatformFooter/index.tsx
+++ src/components/PlatformFooter/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 
-
 const Footer = ({ lang }: { lang: string }) => {
   // Add dictionary state
   const [dictionary, setDictionary] = useState<any>({});
@@ -19,13 +18,13 @@ const Footer = ({ lang }: { lang: string }) => {
       className="wow fadeInUp fixed bottom-0 left-0 right-0 z-10 bg-[#090E34]"
       data-wow-delay=".15s"
     >
-      <div className="border-t border-[#8890A4] border-opacity-40 py-8">
+      <div className="border-t border-[#8890A4] border-opacity-40 py-1">
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4 md:w-2/3 lg:w-1/2"></div>
             <div className="w-full px-4 md:w-1/3 lg:w-1/2">
-              <div className="my-1 flex justify-center md:justify-end">
-                <p className="text-base text-gray-7">
+              <div className="flex justify-center font-thin md:justify-end">
+                <p className="text-xs text-gray-7">
                   © 2024{" "}
                   <a
                     href="https://modastera.com"
