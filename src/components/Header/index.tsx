b0bf48ff1db560bd602520420bbf8f860bac0dcc
Joshua Owoyemi <tjosh.owoyemi@gmail.com>
2024-12-13 12:54:44 +0900

    update header and hero section

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,6 @@ import LanguageSwitcher from "./LanguageSwitcher";
 import menuData from "./menuData";
 
 const Header = ({ lang }: { lang: string }) => {
-
   // Add dictionary state
   const [dictionary, setDictionary] = useState<any>({});
 
@@ -60,7 +59,6 @@ const Header = ({ lang }: { lang: string }) => {
         className={`ud-header left-0 top-0 z-40 flex w-full items-center ${
           sticky
             ? "shadow-nav fixed z-[999] border-b border-stroke bg-white/80 backdrop-blur-[15px] dark:border-dark-3/20 dark:bg-dark/50"
-            // : "absolute bg-transparent"
             : "fixed z-[999] bg-white/80 backdrop-blur-[15px] dark:bg-dark/50"
         }`}
       >
@@ -114,8 +112,8 @@ const Header = ({ lang }: { lang: string }) => {
                 )}
               </Link>
             </div>
-            <div className="flex w-full items-center justify-between px-4">
-              <div>
+            <div className="flex w-full items-center justify-center px-4">
+              <div className="flex flex-1 justify-center">
                 <button
                   onClick={navbarToggleHandler}
                   id="navbarToggler"
@@ -158,7 +156,7 @@ const Header = ({ lang }: { lang: string }) => {
                       : "invisible top-[120%] opacity-0"
                   }`}
                 >
-                  <ul className="block lg:ml-8 lg:flex lg:gap-x-8 xl:ml-14 xl:gap-x-12">
+                  <ul className="block lg:flex lg:items-center lg:gap-x-8 xl:gap-x-12">
                     {menuData.map((menuItem, index) =>
                       menuItem.path ? (
                         <li key={index} className="group relative">
@@ -191,7 +189,6 @@ const Header = ({ lang }: { lang: string }) => {
                             </Link>
                           )}
                         </li>
-
                       ) : (
                         <li className="submenu-item group relative" key={index}>
                           {pathUrl !== "/" ? (
@@ -266,49 +263,43 @@ const Header = ({ lang }: { lang: string }) => {
                             ))}
                           </div>
                         </li>
-                      )
+                      ),
                     )}
-                    <div className="flex h-[55px] items-center px-2">
-                      <Link
-                        href="https://docs.google.com/forms/d/1Ca9TLkSCnErycT54DUk2ZhzKlyOGakktV6S3m9gzSeg/viewform?edit_requested=true"
-                        className="mt-4 inline-block rounded-md border border-transparent bg-secondary px-6 py-1 text-center text-base font-medium text-white transition hover:bg-[#0BB489]"
-                      >
-                        {dictionary?.header?.waitlistBtnTitle}
-                      </Link>
-                    </div>
                   </ul>
                 </nav>
               </div>
-              <div className="hidden items-center justify-end pr-16 sm:flex lg:pr-0">
-                <div>
+              <div className="flex items-center justify-end">
+                <div className="hidden lg:block">
                   <LanguageSwitcher currentLang={lang} />
                 </div>
-                <div>
+                <div className="hidden lg:block">
                   {/* theme toggler */}
                   <button
                     aria-label="theme toggler"
-                    onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                    onClick={() =>
+                      setTheme(theme === "dark" ? "light" : "dark")
+                    }
                     className="flex h-8 w-8 items-center justify-center text-body-color duration-300 dark:text-white"
                   >
-                  <span>
-                    <svg
-                      viewBox="0 0 16 16"
-                      className="hidden h-[22px] w-[22px] fill-current dark:block"
-                    >
-                      <path d="M4.50663 3.2267L3.30663 2.03337L2.36663 2.97337L3.55996 4.1667L4.50663 3.2267ZM2.66663 7.00003H0.666626V8.33337H2.66663V7.00003ZM8.66663 0.366699H7.33329V2.33337H8.66663V0.366699V0.366699ZM13.6333 2.97337L12.6933 2.03337L11.5 3.2267L12.44 4.1667L13.6333 2.97337ZM11.4933 12.1067L12.6866 13.3067L13.6266 12.3667L12.4266 11.1734L11.4933 12.1067ZM13.3333 7.00003V8.33337H15.3333V7.00003H13.3333ZM7.99996 3.6667C5.79329 3.6667 3.99996 5.46003 3.99996 7.6667C3.99996 9.87337 5.79329 11.6667 7.99996 11.6667C10.2066 11.6667 12 9.87337 12 7.6667C12 5.46003 10.2066 3.6667 7.99996 3.6667ZM7.33329 14.9667H8.66663V13H7.33329V14.9667ZM2.36663 12.36L3.30663 13.3L4.49996 12.1L3.55996 11.16L2.36663 12.36Z" />
-                    </svg>
+                    <span>
+                      <svg
+                        viewBox="0 0 16 16"
+                        className="hidden h-[22px] w-[22px] fill-current dark:block"
+                      >
+                        <path d="M4.50663 3.2267L3.30663 2.03337L2.36663 2.97337L3.55996 4.1667L4.50663 3.2267ZM2.66663 7.00003H0.666626V8.33337H2.66663V7.00003ZM8.66663 0.366699H7.33329V2.33337H8.66663V0.366699V0.366699ZM13.6333 2.97337L12.6933 2.03337L11.5 3.2267L12.44 4.1667L13.6333 2.97337ZM11.4933 12.1067L12.6866 13.3067L13.6266 12.3667L12.4266 11.1734L11.4933 12.1067ZM13.3333 7.00003V8.33337H15.3333V7.00003H13.3333ZM7.99996 3.6667C5.79329 3.6667 3.99996 5.46003 3.99996 7.6667C3.99996 9.87337 5.79329 11.6667 7.99996 11.6667C10.2066 11.6667 12 9.87337 12 7.6667C12 5.46003 10.2066 3.6667 7.99996 3.6667ZM7.33329 14.9667H8.66663V13H7.33329V14.9667ZM2.36663 12.36L3.30663 13.3L4.49996 12.1L3.55996 11.16L2.36663 12.36Z" />
+                      </svg>
 
-                    <svg
-                      viewBox="0 0 23 23"
-                      className={`h-[30px] w-[30px] fill-current text-dark dark:hidden ${
-                        !sticky && pathUrl === "/" && "text-white"
-                      }`}
-                    >
-                      <g clipPath="url(#clip0_40_125)">
-                        <path d="M16.6111 15.855C17.591 15.1394 18.3151 14.1979 18.7723 13.1623C16.4824 13.4065 14.1342 12.4631 12.6795 10.4711C11.2248 8.47905 11.0409 5.95516 11.9705 3.84818C10.8449 3.9685 9.72768 4.37162 8.74781 5.08719C5.7759 7.25747 5.12529 11.4308 7.29558 14.4028C9.46586 17.3747 13.6392 18.0253 16.6111 15.855Z" />
-                      </g>
-                    </svg>
-                  </span>
+                      <svg
+                        viewBox="0 0 23 23"
+                        className={`h-[30px] w-[30px] fill-current text-dark dark:hidden ${
+                          !sticky && pathUrl === "/" && "text-white"
+                        }`}
+                      >
+                        <g clipPath="url(#clip0_40_125)">
+                          <path d="M16.6111 15.855C17.591 15.1394 18.3151 14.1979 18.7723 13.1623C16.4824 13.4065 14.1342 12.4631 12.6795 10.4711C11.2248 8.47905 11.0409 5.95516 11.9705 3.84818C10.8449 3.9685 9.72768 4.37162 8.74781 5.08719C5.7759 7.25747 5.12529 11.4308 7.29558 14.4028C9.46586 17.3747 13.6392 18.0253 16.6111 15.855Z" />
+                        </g>
+                      </svg>
+                    </span>
                   </button>
                 </div>
               </div>
